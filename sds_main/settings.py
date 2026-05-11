349efd1756681e876f0064e71779154fe200278e
vishalkubde01 <kubdevishal33@gmail.com>
2026-05-11 14:15:46 +0530

    Student Portal

--- sds_main/settings.py
+++ sds_main/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'sds.context_processors.student_portal_features',
             ],
         },
     },
