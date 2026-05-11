5aaa3e36b82c0856c7315a684bf343aea6f56e7b
vishalkubde01 <kubdevishal33@gmail.com>
2026-05-11 21:39:03 +0530

    settings

--- sds_main/settings.py
+++ sds_main/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)-#wj42wfm88kx+@@_pc2++83-7f9yls%kw2l+n8kfq6fas6u*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
 "147.93.29.224",
