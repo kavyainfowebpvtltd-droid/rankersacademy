585572086bfddab69017442e6932600a4f6c17a6
Nayan Dakhole <yourgithubemail@example.com>
2026-05-12 14:45:15 +0530

    Settings changed

--- sds_main/settings.py
+++ sds_main/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-  'sds_main/static',
+    BASE_DIR / 'sds_main' / 'static',
 ]
 
 MEDIA_URL = '/media/'
